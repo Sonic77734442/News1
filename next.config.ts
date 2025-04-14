cd9f2122876125a7a13f64ae6e6ffd7e99251965
Sonic77734442 <102771205+Sonic77734442@users.noreply.github.com>
2025-04-14 18:23:54 +0500

    Fix: removed deprecated swcMinify from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['cdn.sanity.io'], // ✅ разрешаем картинки с Sanity CDN
+    domains: ['cdn.sanity.io'],
   },
 }
 
