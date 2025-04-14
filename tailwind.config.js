dd598f43101114b8e0023fdc62a54b13b0d9e88b
Sonic77734442 <102771205+Sonic77734442@users.noreply.github.com>
2025-04-14 18:28:16 +0500

    Fix: resolve merge conflict in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // 👈 включаем поддержку тем
-<<<<<<< HEAD
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './studio/**/*.{js,ts,jsx,tsx,ts}'
   ],
-=======
- content: [
-  './pages/**/*.{js,ts,jsx,tsx}',
-  './components/**/*.{js,ts,jsx,tsx}',
-  './app/**/*.{js,ts,jsx,tsx}',
-  './studio/**/*.{js,ts,jsx,tsx,ts}', // если ты используешь интерфейсы внутри studio
-],
->>>>>>> 319e0e7 (Initial commit)
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
