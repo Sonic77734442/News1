d191513c44d7ec5fa2f08a35537e9119a0f33258
Sonic77734442 <102771205+Sonic77734442@users.noreply.github.com>
2025-04-15 15:34:42 +0500

    Подключены GTM, CookieYes, Metrika и Pixel. Готово!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import Analytics from '@/components/Analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      <Analytics />
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ru">
       <Head />
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WJH6SMCV"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
