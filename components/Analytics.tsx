4de37ff0777e1109067a5b4eeaf6f259bf3ec493
Sonic77734442 <102771205+Sonic77734442@users.noreply.github.com>
2026-01-25 21:37:32 +0500

    Add Facebook Pixel ID

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -109,7 +109,7 @@ export default function Analytics() {
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', 'YOUR_PIXEL_ID');
+            fbq('init', '559436252743503');
             fbq('track', 'PageView');
           `,
         }}
