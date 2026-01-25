cb2f1f3f7b8856e84c989f5f2773a1afab4ce7ea
Sonic77734442 <102771205+Sonic77734442@users.noreply.github.com>
2026-01-25 22:00:40 +0500

    Add GTM consent mode default/update

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -50,10 +50,45 @@ export default function Analytics() {
     }
   }, [enabled, consent]);
 
-  if (!enabled || !ready || consent !== 'granted') return null;
+  useEffect(() => {
+    if (!enabled) return;
+    try {
+      (window as any).dataLayer = (window as any).dataLayer || [];
+      const gtag = (...args: any[]) => (window as any).dataLayer.push(args);
+      if (consent === 'granted') {
+        gtag('consent', 'update', {
+          ad_storage: 'granted',
+          analytics_storage: 'granted',
+        });
+      } else if (consent === 'denied') {
+        gtag('consent', 'update', {
+          ad_storage: 'denied',
+          analytics_storage: 'denied',
+        });
+      }
+    } catch {}
+  }, [enabled, consent]);
+
+  if (!enabled) return null;
 
   return (
     <>
+      <Script
+        id="gtm-consent-default"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('consent', 'default', {
+              ad_storage: 'denied',
+              analytics_storage: 'denied',
+              wait_for_update: 500
+            });
+          `,
+        }}
+      />
+
       {/* Google Tag Manager */}
       <Script
         id="gtm-script"
@@ -70,30 +105,32 @@ export default function Analytics() {
       />
 
       {/* Yandex Metrika */}
-      <Script
-        id="metrika"
-        strategy="lazyOnload"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(m,e,t,r,i,k,a){
-              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) { return; }
-              }
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      {ready && consent === 'granted' && (
+        <Script
+          id="metrika"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            ym(101118405, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:false
-            });
-          `,
-        }}
-      />
+              ym(101118405, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:false
+              });
+            `,
+          }}
+        />
+      )}
 
       {/* Facebook Pixel is managed via GTM */}
     </>
